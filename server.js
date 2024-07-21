2b77ea0f145a9ab3e81838274de5fa0c3b5db663
StoicTylerDurden <buhussayyen@gmail.com>
2024-07-21 14:11:29 +0300

    Connected to database

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-// Here is where we import modules
-// We begin by loading Express
+//Imported modules
+const dotenv = require("dotenv").config(); 
 const express = require("express");
+const morgan = require('morgan');
+// const mongoose = require('mongoose'); This is not needed anymore bec i have config folder
 
+//Database call
+require('./config/database.js');
+
+// dotenv.config(); This not needed
 const app = express();
+
+//Middleware
+app.use(morgan('dev'));
+
+
+
+
+//Routes
+
+//Landing Page
+app.get("/", (req, res, next) => {
+    res.render("index.ejs");
+  });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
